b77156eb0dbb70c02ace3fc4a85ce526aad5dcb7
Laurence Drake <laurencedrake@gmail.com>
2015-05-28 06:56:45 -0500

    Replace NameSpaces with call to Scopes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
+  namespace :api, defaults: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1) do
       resources :products
     end
-    namespace :v2 do
+    scope module: :v2, constraints: ApiConstraints.new(version: 2, default: true) do
       resources :products
     end
   end
